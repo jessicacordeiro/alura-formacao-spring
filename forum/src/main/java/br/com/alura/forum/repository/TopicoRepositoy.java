82017756b878b14e10bd3f4a543b32be24532c61
jessicacordeiro <jessica.cordeiro121@gmail.com>
2021-07-10 20:58:09 -0300

    Criação do CRUD completo do Curso - Spring Boot Contrua uma Api Rest.

---
+++ forum/src/main/java/br/com/alura/forum/repository/TopicoRepositoy.java
@@ -0,0 +1,13 @@
+package br.com.alura.forum.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.alura.forum.modelo.Topico;
+
+public interface TopicoRepositoy extends JpaRepository<Topico, Long> {
+
+	List<Topico> findByCursoNome(String nomeCurso);
+	
+}
