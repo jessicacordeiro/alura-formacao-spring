d319c1621cdd25632a3e66d140660e873989fe5c
jessicacordeiro <jessica.cordeiro121@gmail.com>
2021-07-11 18:51:20 -0300

    Colocando o pacote de monitoramento Spring Boot Admin

---
+++ spring-boot-adm/src/test/java/br/com/alura/springbootadm/SpringBootAdmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.springbootadm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
