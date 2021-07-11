8b35c229f5fc4beb65fe8b38e4b578dbacf34f17
jessicacordeiro <jessica.cordeiro121@gmail.com>
2021-07-11 17:44:01 -0300

    Validando um token

--- forum/src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
+++ forum/src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -34,7 +34,7 @@ public class AutenticacaoController {
 		try {
 			Authentication authentication = authManager.authenticate(dadosLogin);
 			String token = tokenService.gerarToken(authentication);
-			return ResponseEntity.ok(new TokenDto(token, "Bearer"));
+			return ResponseEntity.ok(new TokenDto(token, "Bearer "));
 		} catch(AuthenticationException e) {
 			return ResponseEntity.badRequest().build();
 		}
